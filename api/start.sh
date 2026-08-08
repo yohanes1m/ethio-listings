f38498b29c95bce75c354841fd871039aaaa2333
yohanes1m <johnyemande@gmail.com>
2026-08-08 03:19:25 +0300

    fix: add Ubuntu lib path to LD_LIBRARY_PATH for Nix Python GDAL deps

--- api/start.sh
+++ api/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Nix Python uses a patched dynamic linker that doesn't search Ubuntu apt library
+# paths by default. Adding /usr/lib/x86_64-linux-gnu makes libcurl.so.4 and other
+# apt-installed GDAL dependencies findable when ctypes loads libgdal.so.
+export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH:-}"
+
 python manage.py migrate --noinput
 python manage.py seed --skip-if-seeded
 
